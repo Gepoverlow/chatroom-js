ca2aa2c2a7bf05fadf0459416c7127d5b79ccd15
lucas <gepeoverload@gmail.com>
2022-07-19 11:41:57 +0200

    first commit- initial folders and files in place

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const http = require("http");
+
+const app = express();
+const clientPath = `${__dirname}/../client`;
+
+app.use(express.static(clientPath));
+
+const server = http.createServer(app);
+
+const port = 8080;
+
+server.listen(port, () => {
+  console.log("server running on " + port);
+});
+
+const io = require("socket.io")(server);
+
+io.on("connection", (socket) => {
+  console.log("someone connected");
+});
