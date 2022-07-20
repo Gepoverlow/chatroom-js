6ca9dc924b9afb87f168e2e6665c97b7442b02ca
lucas <gepeoverload@gmail.com>
2022-07-20 11:14:57 +0200

    aligned the form inside the chatbow correctly

--- src/dom-stuff/chatMessageRender.js
+++ src/dom-stuff/chatMessageRender.js
@@ -3,24 +3,25 @@ function chatMessageRender(parent) {
   chatMessage.id = "container-chatmessage";
   parent.appendChild(chatMessage);
 
-  const chatMessageBox = document.createElement("div");
-  chatMessageBox.className = "input-group";
-  chatMessage.appendChild(chatMessageBox);
-
   const chatMessageForm = document.createElement("form");
   chatMessageForm.id = "message-form";
-  chatMessageBox.appendChild(chatMessageForm);
+  chatMessageForm.className = "form-inline";
+  chatMessage.appendChild(chatMessageForm);
+
+  const chatMessageBox = document.createElement("div");
+  chatMessageBox.className = "input-group";
+  chatMessageForm.appendChild(chatMessageBox);
 
   const chatMessageInput = document.createElement("input");
   chatMessageInput.className = "form-control";
   chatMessageInput.id = "message-input";
-  chatMessageForm.appendChild(chatMessageInput);
+  chatMessageBox.appendChild(chatMessageInput);
 
   const chatMessageButton = document.createElement("button");
   chatMessageButton.className = "btn btn-primary";
   chatMessageButton.id = "submit-message-button";
   chatMessageButton.textContent = "Submit Message";
-  chatMessageForm.appendChild(chatMessageButton);
+  chatMessageBox.appendChild(chatMessageButton);
 }
 
 export { chatMessageRender };
